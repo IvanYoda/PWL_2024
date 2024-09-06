976ff441dcfd031b5f42f4dff17c24db11696f93
IvanYoda <ivanyodaazul@gmail.com>
2024-09-06 09:28:01 +0700

    praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'World';
+   });
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '2241760094';
+   });
+
+   Route::get('/user/{name}', function ($name) { 
+    return 'Nama saya Muhammad Ivan Yoda Bellamy'; 
+    });
+
+    Route::get('/posts/{post}/comments/{comment}', function 
+    ($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+    }); 
+
+    Route::get('/articles/{id}', function ($id) {
+        return 'Halaman Artikel dengan ID ' . $id;
+    });
+
+    Route::get('/user/{name?}', function ($name='Ivan') {
+        return 'Nama saya '.$name; 
+        });
+        
